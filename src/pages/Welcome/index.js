b8da53bc17743cf02d1339986954c6bd36372db2
ZeroWhisper <marcos.devel@gmail.com>
2019-12-30 17:37:02 -0300

    first commit

---
+++ src/pages/Welcome/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+// import styles from './style';
+
+class Welcome extends Component {
+  render() {
+    return <div>TESTE WELCOME</div>;
+  }
+}
+
+export default Welcome;
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import Welcome from "./Welcome";
+
+class Main extends Component {
+  render() {
+    return <Welcome />;
+  }
+}
+
+export default Main;
