69b95f14f1f4a6282cac1c75cf97cb3b90162394
ZeroWhisper <marcos.devel@gmail.com>
2020-01-02 13:17:02 -0300

    work with another plugin

--- src/components/AppContainer/style.js
+++ src/components/AppContainer/style.js
@@ -4,4 +4,6 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   flex: 1;
+  border: 1px solid red;
+  height: 100%;
 `;
--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
+import { Loop, Stage, World, Body } from "react-game-kit";
 
 import AppContainer from "~/components/AppContainer";
+import Actor from "./components/Actor";
 
 import { Container } from "./style";
 
@@ -8,7 +10,23 @@ class Welcome extends Component {
   render() {
     return (
       <AppContainer>
-        <Container>TESTE</Container>
+        <Container>
+          <Loop>
+            <Stage style={{ background: "#3a9bdc" }} width={1024} height={576}>
+              <World
+                gravity={{
+                  x: 0,
+                  y: 1,
+                  scale: 0.001
+                }}
+              >
+                <Body>
+                  <Actor />
+                </Body>
+              </World>
+            </Stage>
+          </Loop>
+        </Container>
       </AppContainer>
     );
   }
