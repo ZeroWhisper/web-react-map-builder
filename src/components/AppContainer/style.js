0aa11704c59e1243f09a68db2706f9a929505b97
ZeroWhisper <marcos.devel@gmail.com>
2020-02-17 23:17:12 -0300

    Atualizando projeto

--- src/components/AppContainer/style.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-// import styled from 'styled-components/native';
-
-export const Container = styled.div`
-  display: flex;
-  flex: 1;
-  border: 1px solid red;
-  height: 100%;
-`;
--- src/pages/Welcome/style.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Container = styled.div`
-  display: flex;
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-  background-color: greenyellow;
-`;
