600b47b41a49e47310baaa5d09b0f8fbf2b800cc
unknown <marcos.devel@gmail.com>
2020-09-05 12:37:53 -0300

    reorder

--- older/src/store/sagas/task/index.js
+++ older/src/store/sagas/task/index.js
@@ -1,10 +1,7 @@
 import { call, take, put } from 'redux-saga/effects';
-// import CJSON from 'circular-json';
 import rsf from '~/service/firebase';
-// import rsf from 'redux-saga-firebase';
 
 import { Creators as TaskCreators } from '~/store/ducks/task';
-// import rsf from '~/config/firebase';
 
 export function* taskChannel() {
   const channel = rsf.firestore.channel('task');
@@ -12,15 +9,7 @@ export function* taskChannel() {
     const task = yield take(channel);
 
     const result = [];
-    task.forEach(doc => result.push({ id: doc.id, ...doc.data() }));
-
-    // const res = [];
-    // task.forEach(doc =>
-    //   res.push({
-    //     id: doc.id,
-    //     ...doc.data(),
-    //   }),
-    // );
+    task.forEach((doc) => result.push({ id: doc.id, ...doc.data() }));
 
     yield put(TaskCreators.taskSuccess(result));
   }
@@ -28,14 +17,10 @@ export function* taskChannel() {
 
 export function* taskAdd({ payload }) {
   yield call(rsf.firestore.addDocument, 'task', payload);
-
-  // console.log(doc.data());
 }
 
 export function* taskDel({ payload }) {
   yield call(rsf.firestore.deleteDocument, `task/${payload}`);
-
-  // console.log(doc.data());
 }
 
 export function* taskUpdate({ payload }) {
@@ -47,30 +32,10 @@ export function* taskUpdate({ payload }) {
 }
 
 export function* taskRequest() {
-  // const userData = {
-  //   title: 'Test12e',
-  //   message: 'test12e',
-  // };
-
   yield call(rsf.firestore.addDocument, 'task', {
     title: 'Elon ' + Date.now(),
     message: 'Musk',
   });
-
-  // yield call(rsf.firestore.addDocument, `users/123`, userData);
-
-  // const doc = yield call(rsf.database.ref('task').push, {
-  //   title: 'Elon',
-  //   message: 'Musk',
-  // });
-
-  // const doc = yield call(
-  //   rsf.database.update,
-  //   `task/${userData.title}`,
-  //   userData,
-  // );
-
-  // console.tron.log('TESTE', doc);
 }
 
 export function* taskSuccess() {}
