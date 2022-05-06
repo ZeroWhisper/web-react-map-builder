8312e61fa7858671f7c3fe1891678ee4db84757a
Marcos <marcos.devel@gmail.com>
2022-05-06 09:30:08 -0300

    Update

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -2,7 +2,7 @@
 
 export default (req, res) => {
   res.statusCode = 200;
-  // Add GOOD FOR TEST 2
+  // Add GOOD FOR
   // console.group('Value Changed');
   // console.log('newValue', field, newValue);
   // console.log(`action: ${actionMeta.action}`);
