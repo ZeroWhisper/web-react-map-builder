82ad8eaba5b4b9284bbacb2022a9fcd92b237bbc
Marcos <marcos.devel@gmail.com>
2022-05-18 10:58:56 -0300

    Remove

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -2,7 +2,6 @@
 
 export default (req, res) => {
   res.statusCode = 200;
-  // Add add
   // console.group('Value Changed');
   // console.log('newValue', field, newValue);
   // console.log(`action: ${actionMeta.action}`);
