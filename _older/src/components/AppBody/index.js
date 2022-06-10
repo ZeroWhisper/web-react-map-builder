d2e6b0941377421e0ddc2f1d2f3932dd64eb8643
ZeroWhisper <marcos.devel@gmail.com>
2022-06-10 10:00:50 -0300

    older

--- _older/src/components/AppBody/index.js
+++ _older/src/components/AppBody/index.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React from 'react';
 
-import { Content } from "./style";
+import { Content } from './style';
 
-const AppBody = props => {
+const AppBody = (props) => {
   return <Content>{props.children}</Content>;
 };
 
